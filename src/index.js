15a221e6ae59e98c79b3c7a0761106bc287ddd6e
Annloveodessa <anjel1112@gmail.com>
2023-06-19 14:17:35 +0200

    data coment

--- src/index.js
+++ src/index.js
@@ -8,19 +8,19 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-const data = {
+// const data = {
   
-  "username": "Jacques Gluke",
-  "tag": "jgluke",
-  "location": "Ocho Rios, Jamaica",
-  "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  "stats": {
-    "followers": 5603,
-    "views": 4827,
-    "likes": 1308
-  }
+//   "username": "Jacques Gluke",
+//   "tag": "jgluke",
+//   "location": "Ocho Rios, Jamaica",
+//   "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+//   "stats": {
+//     "followers": 5603,
+//     "views": 4827,
+//     "likes": 1308
+//   }
 
-};
+// };
 // const profile =(
 // <div class="profile">
 //   <div class="description">
